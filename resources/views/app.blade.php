dbe36d7a45fba480df63c2101ed1061d8451b94d
ririki <ririki727@gmail.com>
2019-09-04 14:44:16 +0900

    icon画像設定

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+		<link rel="apple-touch-icon" href="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwinvfCBu7bkAhVJCqYKHej2BAcQjRx6BAgBEAQ&url=http%3A%2F%2Fwww.appbank.net%2F2012%2F06%2F21%2Fiphone-application%2F430459.php&psig=AOvVaw2ijgbwS5INpmDRgTJeVPJ7&ust=1567662215726781" />
 	</head>
 	<body>
 		<nav class="navbar navbar-default">
